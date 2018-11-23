8026c33e6d8c15309d1bd83c5ea377fdcd67611d
franck <carnet.franck.paul@gmail.com>
2018-11-23 11:31:37 +0100

    Switch to JSON for js data (Dotclear 2.15+)

--- _define.php
+++ _define.php
@@ -17,9 +17,9 @@ $this->registerModule(
     "Writers",                             // Name
     "Invite people to write on your blog", // Description
     "Olivier Meunier",                     // Author
-    '1.2.1',                               // Version
+    '1.4',                                 // Version
     [
-        'requires'    => [['core', '2.13']], // Dependencies
+        'requires'    => [['core', '2.15']], // Dependencies
         'permissions' => 'admin',            // Permissions
         'type'        => 'plugin',           // Type
         'details'     => 'http://plugins.dotaddict.org/dc2/details/writers'
---
+++ js/writers.js
@@ -0,0 +1,14 @@
+/*global $, usersList */
+'use strict';
+
+$(function() {
+  const i_id = $('#i_id');
+  if (i_id.length) {
+    const usersList = getData('writers');
+    i_id.autocomplete(usersList, {
+      delay: 1000,
+      matchSubset: true,
+      matchContains: true
+    });
+  }
+});
