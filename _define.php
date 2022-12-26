cb384800de9ae98bff84bd32c8cae7ece83c74fa
franck <carnet.franck.paul@gmail.com>
2022-12-26 22:06:32 +0100

    Code review

--- _define.php
+++ _define.php
@@ -18,17 +18,17 @@ $this->registerModule(
     'Writers',
     'Invite people to write on your blog',
     'Olivier Meunier',
-    '2.0',
+    '2.1',
     [
         'requires'    => [['core', '2.24']],
         'permissions' => dcCore::app()->auth->makePermissions([
             dcAuth::PERMISSION_ADMIN,
         ]),
-        'type'     => 'plugin',
-        'settings' => [],
+        'type'        => 'plugin',
+        'settings'    => [],
 
-        'details'    => 'https://plugins.dotaddict.org/dc2/details/writers',
-        'support'    => 'https://github.com/franck-paul/writers',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/writers/master/dcstore.xml',
+        'details'     => 'https://plugins.dotaddict.org/dc2/details/writers',
+        'support'     => 'https://github.com/franck-paul/writers',
+        'repository'  => 'https://raw.githubusercontent.com/franck-paul/writers/master/dcstore.xml',
     ]
 );
