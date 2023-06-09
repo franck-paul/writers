34df91a8e6de1dcf6f1f0aa407d572fa795d0fe3
franck <carnet.franck.paul@gmail.com>
2023-06-09 19:05:28 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,17 +12,17 @@
 use Dotclear\Helper\L10n;
 
 L10n::$locales['Writers'] = 'Rédacteurs';
-L10n::$locales['writers'] = 'Rédacteurs';
+L10n::$locales['writers'] = 'writers';
 L10n::$locales['Writer does not exists.'] = 'Le rédacteur n\'existe pas.';
 L10n::$locales['You cannot add or update this writer.'] = 'Vous ne pouvez pas ajouter ou modifier ce rédacteur.';
 L10n::$locales['You cannot change your own permissions.'] = 'Vous ne pouvez pas changer vos propres permissions.';
-L10n::$locales['Permissions updated for user %s'] = '';
+L10n::$locales['Permissions updated for user %s'] = 'Permissions mises à jour pour l\'utilisatrice ou l\'utilisateur %s';
 L10n::$locales['Active writers'] = 'Rédacteurs actifs';
 L10n::$locales['No writers'] = 'Aucun rédacteur';
 L10n::$locales['change permissions'] = 'changer les permissions';
 L10n::$locales['Invite a new writer'] = 'Inviter un nouveau rédacteur';
 L10n::$locales['Author ID (login): '] = 'Identifiant utilisateur (login) : ';
 L10n::$locales['Invite'] = 'Inviter';
-L10n::$locales['Back'] = '';
+L10n::$locales['Back'] = 'Retour';
 L10n::$locales['You are about to set permissions on the blog %s for user %s (%s).'] = 'Vous allez définir les permissions de l\'utilisateur %2$s (%3$s) sur le blog %1$s';
-L10n::$locales['Save'] = '';
+L10n::$locales['Save'] = 'Enregistrer';
