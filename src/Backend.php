062d1cc6c3ac5110e35afb2c1cdf59cccfc6c089
franck <carnet.franck.paul@gmail.com>
2024-12-26 16:03:06 +0100

    PHP Code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief writers, a plugin for Dotclear 2
  *
@@ -22,7 +23,7 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('writers') . __('Writers');
+        __('writers');
 
         return self::status(My::checkContext(My::BACKEND));
     }
