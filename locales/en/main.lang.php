343363703cf7740f8bffa76845317420fae745f8
franck <carnet.franck.paul@gmail.com>
2025-04-27 16:10:46 +0200

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -10,18 +10,18 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Writers'] = '';
 L10n::$locales['writers'] = '';
 L10n::$locales['Writer does not exists.'] = '';
 L10n::$locales['You cannot add or update this writer.'] = '';
 L10n::$locales['You cannot change your own permissions.'] = '';
 L10n::$locales['Permissions updated for user %s'] = '';
-L10n::$locales['Active writers'] = '';
 L10n::$locales['No writers'] = '';
+L10n::$locales['Permissions:'] = '';
 L10n::$locales['change permissions'] = '';
+L10n::$locales['Active writers'] = '';
 L10n::$locales['Invite a new writer'] = '';
 L10n::$locales['Author ID (login): '] = '';
 L10n::$locales['Invite'] = '';
 L10n::$locales['Back'] = '';
-L10n::$locales['You are about to set permissions on the blog %s for user %s (%s).'] = '';
+L10n::$locales['You are about to set permissions on the blog %1$s for user %2$s (%3$s).'] = '';
 L10n::$locales['Save'] = '';
