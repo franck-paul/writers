8e0a0c2717d23abe6f21d8ddfb6d0dfcd0c0cd66
franck <carnet.franck.paul@gmail.com>
2025-09-22 10:21:25 +0200

    New release 6.1

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Writers',
     'Invite people to write on your blog',
     'Olivier Meunier',
-    '6.0',
+    '6.1',
     [
-        'date'        => '2025-09-07T15:40:06+0200',
+        'date'        => '2025-09-22T10:21:25+0200',
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
