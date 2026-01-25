c64e2d4028192cf54bcc9704bf85b93a8f29c918
franck <carnet.franck.paul@gmail.com>
2026-01-25 11:08:56 +0100

    French locales

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,11 +10,13 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['writers'] = 'writers';
+L10n::$locales['Writers'] = 'Rédacteurs';
+L10n::$locales['Invite people to write on your blog'] = 'Invitez des personnes à écrire sur votre blog';
 L10n::$locales['Writer does not exists.'] = 'Le rédacteur n\'existe pas.';
 L10n::$locales['You cannot add or update this writer.'] = 'Vous ne pouvez pas ajouter ou modifier ce rédacteur.';
 L10n::$locales['You cannot change your own permissions.'] = 'Vous ne pouvez pas changer vos propres permissions.';
 L10n::$locales['Permissions updated for user %s'] = 'Permissions mises à jour pour l\'utilisatrice ou l\'utilisateur %s';
+L10n::$locales['writers'] = 'writers';
 L10n::$locales['No writers'] = 'Aucun rédacteur';
 L10n::$locales['Permissions:'] = 'Permissions :';
 L10n::$locales['change permissions'] = 'changer les permissions';
