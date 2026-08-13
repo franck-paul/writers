90978665c0f5e2999ba9c4537d4318c4878fe94e
franck <contact@open-time.net>
2026-08-13 16:48:04 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,21 +11,25 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'Writers',
-    'Invite people to write on your blog',
-    'Olivier Meunier',
-    '7.0',
-    [
-        'date'        => '2026-08-03T10:17:05+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'settings'    => [],
+declare(strict_types=1);
 
-        'details'    => 'https://dotclear.org/plugin/detail/writers',
-        'support'    => 'https://github.com/franck-paul/writers',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/writers/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Writers',
+        'Invite people to write on your blog',
+        'Olivier Meunier',
+        '7.0',
+        [
+            'date'        => '2026-08-03T10:17:05+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'settings'    => [],
+
+            'details'    => 'https://dotclear.org/plugin/detail/writers',
+            'support'    => 'https://github.com/franck-paul/writers',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/writers/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
